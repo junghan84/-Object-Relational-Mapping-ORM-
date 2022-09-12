2bf929ee64276d26d4c8950a4e182ff5af248a17
JungHan Seo <junghanseo84@gmail.com>
2022-09-12 14:40:24 -0700

    create index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belingTo(Category,{
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Product.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belingToMany(Tag, {
+  through: ProductTag,
+  foreignKey:'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belingToMany(Product,{
+  through: ProductTag,
+  foreignKey:'tag_id',
+});
 
 module.exports = {
   Product,
